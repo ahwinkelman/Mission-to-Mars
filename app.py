8a2fb1cea60006a66d37e30c7c32226503d37f8d
Andrew Winkelman <ahwinkelman@gmail.com>
2022-08-11 19:10:07 -0500

    commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
+import bs4 as bs
 import scraping
+
 
 #flask name
 app = Flask(__name__)
@@ -14,12 +16,12 @@ def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
-   @app.route("/scrape")
+@app.route("/scrape")
 def scrape():
-   mars = mongo.db.mars
-   mars_data = scraping.scrape_all()
-   mars.update_one({}, {"$set":mars_data}, upsert=True)
-   return redirect('/', code=302)
+     mars = mongo.db.mars
+     mars_data = scraping.scrape_all()
+     mars.update_one({}, {"$set":mars_data}, upsert=True)
+     return redirect('/', code=302)
 
-   if __name__ == "__main__":
-   app.run()
+if __name__ == "__main__":
+     app.run()
